dea6a41d4efea2cca079fe837749bc27039415cb
Mitch Winkel-Davis <mitch@mitchwd.com>
2012-10-06 01:25:45 +0930

    Add references & validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :entry
+  
   attr_accessible :user_id, :entry_id
+  validates_presence_of :user_id, :entry_id
+  validates_uniqueness_of :user_id, :scope => :entry_id, :message => "has already completed this entry."
+  
 end
