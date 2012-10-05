5c2245ff76f015f23f3f44ae642bcf1d58d80682
Mitch Winkel-Davis <mitch@mitchwd.com>
2012-10-05 17:24:21 +0930

    Created empty rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liveitapp::Application.config.secret_token = 'f357e280835e886af0fe73e907319bcef1724ba30052a1467dadeae2f161ca6194e528fcf4d42c40f004010e42040871dddfdfe30f02d7edb6ef8dbb08478ae4'
