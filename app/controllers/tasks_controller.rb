c656eaa8b3b32aeefc6f5301a12fb0df27b8001b
Mitch Winkel-Davis <mitch@mitchwd.com>
2012-10-06 18:14:15 +0930

    Fixed task redirections

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,8 +24,8 @@ class TasksController < ApplicationController
 
       respond_to do |format|
         if @task.save
-          format.html { redirect_to @task, notice: 'Task was successfully created.' }
-          format.json { render json: @task, status: :created, location: @task }
+          format.html { redirect_to entry_path(@task.entry_id), notice: 'Task was successfully created.' }
+          format.json { render json: @task.entry, status: :created, location: @task.entry }
         else
           format.html { redirect_to entry_path(@task.entry_id), notice: "You have already completed this entry." }
           format.json { render json: @task.errors, status: :unprocessable_entity }
