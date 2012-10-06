703f111112494f7d6cf415cb9d23372d89bd4955
Mitch Winkel-Davis <mitch@mitchwd.com>
2012-10-06 16:09:57 +0930

    Add db:seeds for entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,104 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+entries = Entry.create([
+  {
+    date: '01/01/2012', 
+    title: "Pause", 
+    author: "Seth", 
+    content: "Take some time today to pause from your business. Spend time today in silence. No iPod, TV, internet or facebook. Simply stop and listen to Christ.", 
+    verse_reference: "Psalm 46:10", 
+    verse_translation: "NLT", 
+    verse_content: "Be still, and know that I am God! I will be honored by every nation. I will be honored throughout the world."
+  },
+  {
+    date: '02/01/2012', 
+    title: "Read", 
+    author: "Seth", 
+    content: "Open up your bible and read a Psalm of your choice today. Read it 3 times, slowly. Each time asking the Holy Spirit to show you something new. Take your time, meditate on it and share it with someone.", 
+    verse_reference: "Psalm 119:27", 
+    verse_translation: "NLT", 
+    verse_content: "Help me understand the meaning of your commandments, and I will meditate on your wonderful deeds."
+  },
+  {
+    date: '03/01/2012', 
+    title: "Share", 
+    author: "Seth", 
+    content: "Today, make an effort to connect with a friend and share what the Lord has been teaching you. Maybe a verse that has stood out to you, or a song that has been on your heart. Take time to share on a spiritual level. And ask them what Gods been teaching them.",
+    verse_reference: "Ruth 4:1", 
+    verse_translation: "NLT", 
+    verse_content: "'Come over here and sit down, friend. I want to talk to you.' So they sat down together."
+  },
+  {
+    date: '04/01/2012', 
+    title: "Hear", 
+    author: "Seth", 
+    content: "Spend the whole afternoon listening to others and not talking about yourself. Refrain from using the words me and I.", 
+    verse_reference: "James 1:19", 
+    verse_translation: "NLT", 
+    verse_content: "Understand this, my dear brothers and sisters: You must all be quick to listen, slow to speak, and slow to get angry."
+  },
+  {
+    date: '05/01/2012', 
+    title: "Action", 
+    author: "Seth", 
+    content: "Send an email (or facebook message) to a missionary working overseas, asking how you can pray for them while also sharing some of your life with them. If you don't know any missionaries, get to know some by asking your friends who they are praying for. Then contact them yourself and pray for Gods work there.", 
+    verse_reference: "Ephesians 4:16", 
+    verse_translation: "NLT", 
+    verse_content: "He makes the whole body fit together perfectly. As each part does its own special work, it helps the other parts grow, so that the whole body is healthy and growing and full of love."
+  },
+  {
+    date: '06/01/2012', 
+    title: "Pause", 
+    author: "Seth", 
+    content: "Set time aside to listen to a sermon (mp3), while you are driving, working, going on a walk or even just lying down to rest. If you have no sermons downloaded, go to 4thseedministries.com/media for free downloads and links. Soak up His teaching.", 
+    verse_reference: "Psalm 119:130", 
+    verse_translation: "NLT", 
+    verse_content: "The teaching of your word gives light, so even the simple can understand."
+  },
+  {
+    date: '07/01/2012', 
+    title: "Plan", 
+    author: "Seth", 
+    content: "Today, open up your Diary/Calendar and lock in a coffee catch up with some one you haven't seen in awhile. Be intentional with in your conversation, encourage them and remind each other of Gods blessings. Organize it today and be sure to follow through.", 
+    verse_reference: "Acts 11:23", 
+    verse_translation: "NLT", 
+    verse_content: "When he arrived and saw this evidence of God's blessing, he was filled with joy, and he encouraged the believers to stay true to the Lord."
+  },
+  {
+    date: '08/01/2012', 
+    title: "Reach Out", 
+    author: "Seth", 
+    content: "Talk to your neighbours, ask how they are going, learn their names (if you don't already know them). Listen to them without talking about yourself the whole time.", 
+    verse_reference: "1 Thessalonians 4:9", 
+    verse_translation: "NLT", 
+    verse_content: "But we don't need to write to you about the importance of loving each other, for God himself has taught you to love one another."
+  },
+  {
+    date: '09/01/2012', 
+    title: "Share", 
+    author: "Seth", 
+    content: "Send a text message to 10 people with a verse that you have recently read. Don't just leave it there. Add a thought provoking question to the text, encouraging dialog on the subject. Together encourage one another through the word.", 
+    verse_reference: "Romans 1:12", 
+    verse_translation: "NLT", 
+    verse_content: "When we get together, I want to encourage you in your faith, but I also want to be encouraged by yours."
+  },
+  {
+    date: '10/01/2012', 
+    title: "Pray", 
+    author: "Seth", 
+    content: "Set aside (at least) 10 minutes today to pray specifically for 3 people you know who are in ministry in your city. Interceding on their behalf, asking Christ to give them wisdom and strength.", 
+    verse_reference: "2 Thessalonians 1:11", 
+    verse_translation: "NLT", 
+    verse_content: "So we keep on praying for you, asking our God to enable you to live a life worthy of his call. May he give you the power to accomplish all the good things your faith prompts you to do."
+  },
+  {
+    date: '11/01/2012', 
+    title: "Awareness", 
+    author: "Seth", 
+    content: "Today, 1) Jump onto google and search for 'Persecuted Church News' 2) Spend time reading stories and praying 3) Share links on Facebook and start conversations.", 
+    verse_reference: "2 Thessalonians 1:5", 
+    verse_translation: "NLT", 
+    verse_content: "And God will use this persecution to show his justice and to make you worthy of his Kingdom, for which you are suffering."
+  }
+])
